2a3ac55c7316509a64f82415dd0c4a66710411de
CaptainJ61 <difigliojimmy@gmail.com>
2020-09-20 19:38:28 -0500

    Prototype3 Finished

---
+++ Prototype3/Assets/Scripts/TriggerZoneAddScore.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * James Difiglio
+ * Prototype3
+ * Counts score for UIManager
+ * 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZoneAddScore : MonoBehaviour
+{
+    private UIManager UIManager;
+
+    private bool triggered = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIManager = GameObject.FindObjectOfType<UIManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            UIManager.score++;
+        }
+    }
+}
