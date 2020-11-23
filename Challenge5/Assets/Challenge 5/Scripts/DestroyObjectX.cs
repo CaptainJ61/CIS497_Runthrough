96b304337ea162ed2e0ff46c7885d8f6cce63acf
CaptainJ61 <difigliojimmy@gmail.com>
2020-11-22 19:47:46 -0600

    Finished Challenge 5

---
+++ Challenge5/Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * James Difiglio
+ * Challenge5
+ * Timer for deleting objects
+ * didnt actually edit this script
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2); // destroy particle after 2 seconds
+    }
+
+
+}
